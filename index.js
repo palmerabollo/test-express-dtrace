f338a5d820aef186f16ccb636c153a220851b8e1
Guido García <guido@tid.es>
2014-06-10 16:34:43 +0200

    express 3.x version

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
-var express = require('express');
+var express = require('express'),
+    crypto = require('crypto'),
+    logger = require('bunyan').createLogger({name: 'myapp'});
+
 var app = express();
 
 app.configure(function () {
-  app.set('view engine', 'ejs');
-  app.set('views', __dirname + '/views');
-
   app.use(express.responseTime());
-  app.use(express.logger());
+  app.use(express.urlencoded());
 
   app.use(app.router);
-
-  app.use(express.errorHandler())
+  app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/', function home(req, res, next) {
-  res.render('index');
+var bubbles = {}; // in-memory store
+
+app.get('/', function home(req, res) {
+  res.redirect('index.html');
+});
+
+app.get('/bubbles', function list(req, res) {
+    res.json(200, bubbles);
+});
+
+app.post('/bubbles', function create(req, res) {
+    var uuid = crypto.randomBytes(20).toString('hex');
+
+    var bubble = req.body; // XXX no validation, please be kind
+    bubble.uuid = uuid;
+    bubble.timestamp = Date.now();
+
+    bubbles[uuid] = bubble;
+    res.json(200, bubble);
+});
+
+app.get('/bubbles/:id', function detail(req, res) {
+    res.json(200, bubbles[req.params.id]);
+});
+
+app.delete('/bubbles/:id', function remove(req, res) {
+    delete bubbles[req.params.id];
+    res.json(204, {});
 });
 
 var server = app.listen(4000, function onstart() {
-  console.log('Ready');
+  logger.info('ready');
 });
+
+setInterval(function cleanup() {
+    logger.debug('run cleanup %d bubbles', Object.keys(bubbles).length);
+    for (key in bubbles) {
+        var expired = bubbles[key].timestamp < Date.now() - 60000;
+        if (expired) {
+            logger.debug('clean expired %s', key);
+            delete bubbles[key];
+        }
+    }
+}, 30000);
